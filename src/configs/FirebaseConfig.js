7d3b28844c560846646f46e373f56899d344f1b7
Ammar_Bareesh <bareesh.ammar00@gmail.com>
2021-12-15 17:08:27 +0300

    first commit

---
+++ src/configs/FirebaseConfig.js
@@ -0,0 +1,13 @@
+const FirebaseConfig = {
+  apiKey: 'AIzaSyAOWoRQGoegkmwnCca7B2GG5GUG_ZP1lcM',
+  authDomain: 'BznsMonster.firebaseapp.com',
+  databaseURL: 'https://BznsMonster.firebaseio.com',
+  projectId: 'BznsMonster',
+  storageBucket: 'BznsMonster.appspot.com',
+  messagingSenderId: '807555350717',
+  appId: '1:807555350717:web:145ba7c21af47203a2f7d4',
+  measurementId: 'G-8KE7HJB252'
+};
+
+export default FirebaseConfig
+
